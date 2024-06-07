a45e9e03b272604cb928620d9f124aae62b1257d
Alireza-mir <157066251+Alireza-mir@users.noreply.github.com>
2024-06-07 19:52:34 +0330

    Add files via upload

---
+++ BST.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarytreeNode A = new BinarytreeNode(30);
+            BinarytreeNode B = new BinarytreeNode(19);
+            BinarytreeNode C = new BinarytreeNode(15);
+            BinarytreeNode D = new BinarytreeNode(11);
+            BinarytreeNode E = new BinarytreeNode(18,C,B);
+            BinarytreeNode F = new BinarytreeNode(25);
+            BinarytreeNode G = new BinarytreeNode(32,A,null);
+            BinarytreeNode H = new BinarytreeNode(14,D,E);
+            BinarytreeNode I = new BinarytreeNode(28,F,G);
+            BinarytreeNode J = new BinarytreeNode(21,H,I);
+
+
+            BinarytreeNode a = new BinarytreeNode(10);
+            BinarytreeNode b = new BinarytreeNode(27);
+            BinarytreeNode c = new BinarytreeNode(38);
+            BinarytreeNode d = new BinarytreeNode(26,a,b);
+            BinarytreeNode e = new BinarytreeNode(31,d,c);
+
+            //BinarytreeNode.merg(J, e);
+            //BinarytreeNode.ADDNode(J, 7);
+            BinarytreeNode.DeleteNode(J, 14);
+            BinarytreeNode.printtree(J);
+            Console.WriteLine("-----------------------");
+            BinarytreeNode.printtree(e);
+            Console.WriteLine("the number after 21 is :"+BinarytreeNode.After(J).data);
+            Console.WriteLine("the number before 21 is :" + BinarytreeNode.Before(J).data);
+            
+            BinarytreeNode.inorder(J);
+            Console.WriteLine("");
+            Console.WriteLine("7th Biggest adad is :" + BinarytreeNode.Kaominadad(J).data);
+
+
+
+
+            MaxHeap node1 = new MaxHeap(10);
+            MaxHeap node2 = new MaxHeap(20);
+            MaxHeap node3 = new MaxHeap(30);
+            MaxHeap node4 = new MaxHeap(40);
+            MaxHeap node5 = new MaxHeap(50);
+
+            List<int> nodes = new List<int>();
+            nodes.Add(node1.number);
+            nodes.Add(node2.number);
+            nodes.Add(node3.number);
+            nodes.Add(node4.number);
+            nodes.Add(node5.number);
+            nodes.Add(BinarytreeNode.Kaominadad(J).data);
+
+            int[] l=nodes.ToArray();
+
+            MaxHeap.BuildMaxHeap(l);
+
+            Console.WriteLine("Maxheap nodes are :");
+            for (int i = 0; i < MaxHeap.HeapSize+1; i++)
+            {
+                Console.Write(" " + l[i]);
+            
+            
+            }
+
+
+
+                Console.ReadKey();
+
+
+        }
+    }
+    class BinarytreeNode
+    {
+        public int data;
+        public BinarytreeNode Left;
+        public BinarytreeNode Right;
+
+        
+
+        public BinarytreeNode(int D)
+        {
+            data = D;
+            Left = null;
+            Right = null;
+        }
+
+        public BinarytreeNode(int D, BinarytreeNode L, BinarytreeNode R)
+        {
+            data = D;
+            Left = L;
+            Right = R;
+        }
+
+        public static BinarytreeNode ADDNode(BinarytreeNode R, int Dataa)
+        {
+            if (R == null)
+            {
+                return new BinarytreeNode(Dataa);
+            }
+            if (Dataa < R.data)
+            {
+                R.Left = ADDNode(R.Left, Dataa);
+            }
+            else if (Dataa > R.data)
+            {
+                R.Right = ADDNode(R.Right, Dataa);
+            }
+
+            return R;
+        }
+
+
+        public static void printtree(BinarytreeNode R, int level = 0)
+        {
+
+            if (R == null) return;
+            printtree(R.Right, level + 1);
+            Console.WriteLine(new string(' ', level * 4) + R.data);
+            printtree(R.Left, level + 1);
+        }
+        public static BinarytreeNode After(BinarytreeNode Node)
+        {
+            return minnode(Node.Right);
+
+        }
+        public static BinarytreeNode Before(BinarytreeNode Node)
+        {
+            return maxnode(Node.Left);
+        }
+       
+        private static BinarytreeNode minnode(BinarytreeNode R)
+        {
+            BinarytreeNode min = R;
+
+            while (min.Left != null)
+            {
+                min = min.Left;
+            }
+            return min;
+
+        }
+        private static BinarytreeNode maxnode(BinarytreeNode R)
+        {
+            BinarytreeNode max = R;
+            while (max.Right != null)
+            {
+                max = max.Right;
+            }
+            return max;
+
+        }
+
+        public static BinarytreeNode DeleteNode(BinarytreeNode R, int Data)
+        {
+            if (R == null)
+                return null;
+            if (Data < R.data)
+            {
+                R.Left = DeleteNode(R.Left, Data);
+            }
+            else if (Data > R.data)
+            {
+                R.Right = DeleteNode(R.Right, Data);
+            }
+            else
+            {
+                //one or no child
+                if (R.Left == null)
+                {
+                    return R.Right;
+                }
+                else if (R.Right == null)
+                {
+                    return R.Left;
+                }            
+                //Two child
+                BinarytreeNode temp = After(R);
+                R.data = temp.data;
+                R.Right = DeleteNode(R.Right, temp.data);
+
+            }
+            return R;
+                
+
+        }
+
+        public static void merg(BinarytreeNode R1,BinarytreeNode R)
+        {
+            if (R == null)
+                return;
+            merg(R1,R.Left);
+            ADDNode(R1, R.data);
+            merg(R1,R.Right);
+        }
+        public static BinarytreeNode Kaominadad(BinarytreeNode R)
+        {
+            List<BinarytreeNode> N = new List<BinarytreeNode>();
+
+            BinarytreeNode.kaominadadHelper(R, N);
+
+            N.Reverse();
+
+            return N[6];
+        
+        
+        }
+        public static void  kaominadadHelper(BinarytreeNode R,List<BinarytreeNode> Node)
+        {
+
+            if (R == null)
+                return ;
+            kaominadadHelper(R.Left, Node);
+            Node.Add(R);
+            kaominadadHelper(R.Right, Node);
+            
+        }
+        public static void inorder(BinarytreeNode R)
+        {
+            if (R == null)
+                return;
+            inorder(R.Left);
+            Console.Write(R.data+" ");
+            inorder(R.Right);
+        }
+
+
+
+    }
+    class MaxHeap
+    {
+     
+        public int number;
+        
+        public static int HeapSize = 0;
+
+        public MaxHeap(int n)
+        {
+            number = n;
+            HeapSize++;
+        
+        }
+        
+        
+        
+        
+        public static void Max_Heapify(int[] A, int i)
+        {
+            int l, r, Bigchild, Temp;
+
+            l = 2 * i;
+            r = 2 * i + 1;
+            if (l < HeapSize && A[l] > A[i])
+                Bigchild = l;
+            else
+                Bigchild = i;
+            if (r < HeapSize && A[r] > A[Bigchild])
+                Bigchild = r;
+
+            if (Bigchild != i)
+            {
+                Temp = A[i];
+                A[i] = A[Bigchild];
+                A[Bigchild] = Temp;
+                Max_Heapify(A, Bigchild);
+            }
+        }
+
+        public static void BuildMaxHeap(int[] A)
+        {
+
+            for (int i = HeapSize / 2; i >=0; i--)
+            {
+                Max_Heapify(A, i);
+            }
+        }
+
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ Maximum priority queue.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_priority_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxHeap person1 = new MaxHeap("Alireza", "mir", 20, "F");
+            MaxHeap person3 = new MaxHeap("mohammad", "mohammadi", 50, "B");
+            MaxHeap person2 = new MaxHeap("zahra", "forghani", 30, "C");
+            MaxHeap person4 = new MaxHeap("abolfazl", "mehrani", 25, "D");
+            MaxHeap person5 = new MaxHeap("nazanin", "aslani", 25, "F");
+            MaxHeap person6 = new MaxHeap("ahmad", "karimi", 30, "A");
+            List<int> list = new List<int>();
+            list.Add(person1.Priority);
+            list.Add(person2.Priority);
+            list.Add(person3.Priority);
+            list.Add(person4.Priority);
+            list.Add(person5.Priority);
+            list.Add(person6.Priority);
+           
+            MaxHeap newPerson1 = new MaxHeap("ali", "mortazavi", 24, "F");
+            list.Add(newPerson1.Priority);
+            MaxHeap newPerson2 = new MaxHeap("mammad", "aghaee", 15, "F");
+            list.Add(newPerson2.Priority);
+            
+            int[] peaple = new int[30];
+            peaple = list.ToArray();
+            
+
+            MaxHeap.BuildMaxHeap(peaple);
+
+            
+            Console.WriteLine(MaxHeap.DeleteFromQueue(peaple));
+            Console.WriteLine(MaxHeap.DeleteFromQueue(peaple));
+            Console.WriteLine(MaxHeap.DeleteFromQueue(peaple));
+            Console.WriteLine(MaxHeap.DeleteFromQueue(peaple));
+
+            
+            Console.Write("priority in array is :");
+
+            for (int i = 0; i < MaxHeap.heapsize; i++)
+            {
+                Console.Write(" " + peaple[i]);
+ 
+            
+            }
+            Console.WriteLine(" ");
+            
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+
+    class MaxHeap
+    {
+        private string Fname;
+        private string Lname;
+        private int Age;
+        private string Skill;
+        public int Priority;
+        public static int heapsize = 0;
+        public MaxHeap(string FN, string LN, int Ag, string Skil)
+        {
+            Fname = FN;
+            Lname = LN;
+            Age = Ag;
+            Skill = Skil;
+
+            if (Skill == "A")
+                Priority = 100 / Age;
+            if (Skill == "B")
+                Priority = 200 / Age;
+            if (Skill == "C")
+                Priority = 300 / Age;
+            if (Skill == "D")
+                Priority = 400 / Age;
+            if (Skill == "E")
+                Priority = 500 / Age;
+            if (Skill == "F")
+                Priority = 600 / Age;
+
+            heapsize++;
+        }
+
+        public static void Max_Heapify(int[] A, int i)
+        {
+            int l, r, Bigchild, Temp;
+
+            l = 2 * i ;
+            r = 2 * i + 1;
+            if (l < heapsize && A[l] > A[i])
+                Bigchild = l;
+            else
+                Bigchild = i;
+            if (r < heapsize && A[r] > A[Bigchild])
+                Bigchild = r;
+
+            if (Bigchild != i)
+            {
+                Temp = A[i];
+                A[i] = A[Bigchild];
+                A[Bigchild] = Temp;
+                Max_Heapify(A, Bigchild);
+            }
+        }
+
+        public static void BuildMaxHeap(int[] A)
+        {
+
+            for (int i = heapsize / 2; i >= 0; i--)
+            {
+                Max_Heapify(A, i);
+            }
+        }
+
+
+
+       
+        public static void AddtoQueue(int[] A, int prio)
+        {
+
+            heapsize++;
+            A[heapsize] = -1000;
+            increasePriority(A, heapsize, prio);
+
+        }
+
+        public static void increasePriority(int[] A, int i, int pr)
+        {
+            int temp = 0;
+            if (pr < A[i])
+            {
+                Console.WriteLine("new priority is smaller than Current priority");
+                return;
+            }
+            A[i] = pr;
+            while (i > 1 && A[i / 2] < A[i])
+            {
+                temp = A[i];
+                A[i] = A[i / 2];
+                A[i / 2] = temp;
+
+                i = i / 2;
+            }
+
+
+        }
+        public static int DeleteFromQueue(int[] A)
+        { 
+            int max = A[0];
+            A[0] = A[heapsize-1];
+            heapsize--;
+            Max_Heapify(A,0);
+            return max;
+        }
+
+    }
+
+}
